0309396bf5d6cf5758e0216ebe2512430f25b0be
israel ramirez <israelramirez.am@gmail.com>
2025-07-27 11:01:59 -0300

    finalizado section2

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,18 +1,49 @@
 import { ImCalendar } from 'react-icons/im'
 import Banner from '../../assets/main_banner.png'
 import styles from './Home.module.css'
-import { TbTargetArrow, TbWorld } from 'react-icons/tb'
+import { TbTargetArrow } from 'react-icons/tb'
 import { TfiWorld } from 'react-icons/tfi'
-import { FaMedal } from 'react-icons/fa'
-
+import { FaMedal, FaHandshake, FaRegLightbulb, FaVideo } from 'react-icons/fa'
+import { BiHeadphone } from 'react-icons/bi'
+import { BiShield } from 'react-icons/bi'
+import { LuZap } from 'react-icons/lu'
 
 export function Home() {
+
+  const services = [
+    {
+      icon: FaHandshake,
+      title: "Asesoría",
+      subtitle: "personalizada",
+    },
+    {
+      icon: LuZap,
+      title: "Agilidad y",
+      subtitle: "Eficiencia",
+    },
+    {
+      icon: BiShield,
+      title: "Compromiso legal",
+      subtitle: "integral",
+    },
+    {
+      icon: BiHeadphone,
+      title: "Confidencialidad",
+      subtitle: "garantizada",
+    },
+    {
+      icon: FaRegLightbulb,
+      title: "Soluciones",
+      subtitle: "a medida",
+    }
+  ];
+
   return (
     <div>
       <div className={styles.bannerContainer}>
         <img src={Banner} alt="BF Law" />
         <div className={styles.bannerContent}>
-          <h2>Impulsamos negocios, Protegemos <span>&nbsp;decisiones</span>.</h2>
+          <h2>Impulsamos negocios, Protegemos <span className={styles.highlight}>&nbsp;decisiones</span>.</h2>
           <p>
             Ofrecemos asesoría legal estratégica con foco en resultados comerciales.<br /><br />
             Asistimos a empresas, startups, inversores y familias en cada paso clave de su camino.
@@ -31,7 +62,7 @@ export function Home() {
                 </h2>
 
                 <h1 className={styles.title}>
-                  BF  LAW es un e<span>studio jurídico co</span>
+                  BF  LAW es un e<span className={styles.highlight}>studio jurídico co</span>
                   n una visión global y enfoque estratégico.
                 </h1>
 
@@ -51,7 +82,7 @@ export function Home() {
             <div className={styles.features}>
               <div className={styles.feature}>
                 <div className={styles.iconWrapper}>
-                <ImCalendar size={30} />
+                  <ImCalendar size={30} />
                 </div>
                 <div>
                   <h3 className={styles.featureTitle}>
@@ -62,7 +93,7 @@ export function Home() {
 
               <div className={styles.feature}>
                 <div className={styles.iconWrapper}>
-                <TfiWorld size={35} />
+                  <TfiWorld size={35} />
                 </div>
                 <div>
                   <h3 className={styles.featureTitle}>
@@ -73,7 +104,7 @@ export function Home() {
 
               <div className={styles.feature}>
                 <div className={styles.iconWrapper}>
-                <TbTargetArrow size={35} />
+                  <TbTargetArrow size={35} />
                 </div>
                 <div>
                   <h3 className={styles.featureTitle}>
@@ -84,7 +115,7 @@ export function Home() {
 
               <div className={styles.feature}>
                 <div className={styles.iconWrapper}>
-                <FaMedal size={30} />
+                  <FaMedal size={30} />
                 </div>
                 <div>
                   <h3 className={styles.featureTitle}>
@@ -96,6 +127,55 @@ export function Home() {
           </div>
         </div>
       </div>
+
+      <section className={styles.section2}>
+        <div className={styles.wrapperSection2}>
+          <div className={styles.section2Top}>
+            <div className={styles.container2}>
+              <div className={styles.header2}>
+                <p className={styles.subtitle}>COMO LO HACEMOS</p>
+
+                <h2 className={styles.title2}>
+                  Brindamos <span className={styles.highlight}>soluciones legales</span>, diseñadas a
+                  medida para cada desafío empresarial.
+                </h2>
+
+                <p className={styles.description2}>
+                  En BF Law, siempre hay una manera. Nuestro equipo se involucra desde el inicio, comprendiendo la
+                  industria, la cultura y los objetivos de cada cliente. Combinamos análisis jurídico riguroso con una
+                  mirada práctica del negocio. Esto nos permite anticipar riesgos, identificar oportunidades y acompañar
+                  procesos clave con precisión.
+                </p>
+
+
+              </div>
+            </div>
+
+            <div className={styles.videoSection}>
+              <div className={styles.playButton}>
+                <FaVideo className={styles.playIcon} />
+                <span className={styles.videoText}>Conoce nuestro estudio</span>
+              </div>
+              <span className={styles.videoMeta}>2:34Min, 14.7MB </span>
+            </div>
+          </div>
+          <div className={styles.section2Bottom}>
+            <div className={styles.servicesFlex}>
+              {services.map((service, index) => {
+                const IconComponent = service.icon;
+                return (
+                  <div key={index} className={styles.serviceCard}>
+                    <div className={styles.iconContainer}>
+                      <IconComponent className={styles.icon} />
+                    </div>
+                    <p className={styles.serviceTitle}>{service.title}</p>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   )
 }
