43cb4a3c5c7a5a8d456c1b9f6d588222b876d29e
israel ramirez <israelramirez.am@gmail.com>
2025-08-01 00:06:19 -0300

    finalizado estudio section

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
-export { Home } from "./Home";
 export { About } from "./About";
 export { Clients } from "./Clients";
 export { Contact } from "./Contact";
+export { Home } from "./Home";
 export { Professionals } from "./Professionals";
